6fbdd06022328104ee8eba4d2ca18db488c7bae5
nickkinn1 <nickkinn4@gmail.com>
2025-10-15 12:51:31 -0500

    Added carousel to homepage

--- site.js
+++ site.js
@@ -44,3 +44,52 @@ favoriteMovies.forEach((movie) => {
 
 //Secret message local storage.
 localStorage.setItem("It's a secret to everybody.", "Yo")
+
+
+
+
+
+//Carousel
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+const prevButton = document.querySelector("#prev")
+const nextButton = document.querySelector("#next")
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+showImages()
+
+setInterval(() => {
+    if(currentImage < urls.length) {
+        currentImage++
+    } else {
+        currentImage = 1;
+    }
+    showImages()
+}, 5000);
+
+prevButton.addEventListener("click", () => {
+    currentImage--
+    showImages()
+})
+
+nextButton.addEventListener("click", () => {
+    currentImage++
+    showImages()
+})
+
+
